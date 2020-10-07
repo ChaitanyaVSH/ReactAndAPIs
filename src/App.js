d0265da627aac0d3bab3bc03d39cf474ce79e264
Chaitanya <chaitu.vsh@gmail.com>
2020-10-07 22:39:03 +0530

    Fetch API to hit the github API, get the data and render the data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          data: data,
+        });
+      });
+  }
+  render() {
+    const users = this.state.data;
+    const styles = {
+      margin: 10,
+      padding: 10,
+      color: "white",
+      backgroundColor: "lightblue",
+      width: "40%",
+      alignItems: "center",
+      justifyContent: "center",
+    };
+
+    return (
+      <div>
+        <h1>
+          {users.map((user) => (
+            <div key={user.id} style={styles}>
+              <h3>Username: {user.login}</h3>
+              <ul>
+                <li>Followers: {user.followers_url.length}</li>
+              </ul>
+            </div>
+          ))}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
