b4dbaa30a559f47e28cd76acae61724306b9812b
Chaitanya <chaitu.vsh@gmail.com>
2020-10-09 23:36:11 +0530

    Implemented a logic to filter the data based on the course ID

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import SearchField from "./components/searchField";
 class App extends Component {
   state = {
     data: [],
-    filteredData: [],
+    filter: false,
+    filterId: "",
     loading: false,
   };
 
@@ -15,16 +16,16 @@ class App extends Component {
     this.setState({
       loading: true,
     });
-    fetch("http://127.0.0.1:5689/api/courses")
+    fetch("http://127.0.0.1:1234/api/courses")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         this.setState({
           data: data,
           loading: false,
         });
       })
       .catch(() => {
+        //https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
         this.setState({
           loading: false,
         });
@@ -43,6 +44,26 @@ class App extends Component {
       boxShadow: "0px 1px 6px orange",
       border: "1px solid orange",
     };
+
+    if (this.state.filter && this.state.loading === false) {
+      const id = parseInt(this.state.filterId);
+      const filteredData = this.state.data.filter((course) => course.id === id);
+
+      return filteredData.length === 0 ? (
+        <h3 style={styles}>No results</h3>
+      ) : (
+        <div>
+          {filteredData.map((course) => (
+            <div key={course.id} style={styles}>
+              <h3>Course ID: {course.id}</h3>
+              <ul>
+                <li>Course Name: {course.name}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      );
+    }
 
     if (this.state.data.length > 0 && this.state.loading === false) {
       return (
@@ -68,25 +89,27 @@ class App extends Component {
     }
 
     return (
-      <h4 style={{ textAlign: "center", margin: "auto" }}>
+      <h4 style={styles}>
         Issue with BackEnd service. Team is notified and working on it.
       </h4>
     );
   }
 
   handleSearch = (id) => {
-    if (id.trim().length === 0)
-      return alert(
-        "Please enter an id in format of number with no extra spaces"
-      );
-
-    //const data = this.state.data;
+    if (isNaN(id) || id.trim().length === 0) {
+      return alert("Please enter a valid number.");
+    }
+    this.setState({
+      filter: true,
+      filterId: parseInt(id),
+    });
   };
   render() {
+    const data = this.getData();
     return (
       <div className="wrapper">
         <SearchField onSearch={this.handleSearch} />
-        {this.getData()}
+        {data}
       </div>
     );
   }
